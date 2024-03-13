9e2c920973f86b0f898a3a67a4c0cbacb8b9505f
Zoltan Borsos <zoltan.borsos1@ibm.com>
2024-03-13 10:25:15 +0100

    gitignore update

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,12 @@ import com.zborsos.dbloaders.db2loader.DB2Loader;
 import com.zborsos.dbloaders.derbyloader.DerbyLoader;
 import com.zborsos.dbloaders.orcleloader.OracleLoader;
 
+import java.util.logging.Logger;
+
+
 public class Main {
+    final static Logger log = Logger.getLogger("DbLoade.project");
+    
     public static void main(String[] args) {
         final String db = "Oracle";
         if(db.equals("DB2")){
@@ -10,12 +15,12 @@ public class Main {
                 DB2Loader dbl = new DB2Loader();
                 dbl.loadRandomRecords(50000);
                 long delCount = dbl.deleteWithEmbeddedSelect();
-                System.out.println("Deleted "+delCount);
+                log.info("Deleted "+delCount);
                 dbl.loadRandomRecords(50000);
                 delCount = dbl.deleteSelectedIDs();
-                System.out.println("Deleted "+delCount);
+                log.info("Deleted "+delCount);
             } catch (Exception e) {
-                System.out.println(e.getMessage());
+                log.info(e.getMessage());
                 throw new RuntimeException(e);
             }
 
@@ -26,12 +31,12 @@ public class Main {
                 OracleLoader dbl = new OracleLoader();
                 dbl.loadRandomRecords(50000);
                 long delCount = dbl.deleteWithEmbeddedSelect();
-                System.out.println("Deleted "+delCount);
+                log.info("Deleted "+delCount);
                 dbl.loadRandomRecords(50000);
                 delCount = dbl.deleteSelectedIDs();
-                System.out.println("Deleted "+delCount);
+                log.info("Deleted "+delCount);
             } catch (Exception e) {
-                System.out.println(e.getMessage());
+                log.info(e.getMessage());
                 throw new RuntimeException(e);
             }
 
